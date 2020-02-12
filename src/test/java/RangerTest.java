5976bc894cd3ee7475ae064067a29b0deb66843e
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-12 09:34:38 +0300

    add test to test instances

--- src/test/java/RangerTest.java
+++ src/test/java/RangerTest.java
@@ -1,2 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class RangerTest {
+
+    @Test
+    public void ranger_instantiatesCorrectly_true() {
+        Ranger testRanger = new Ranger("Rock Stock", "Nyayo");
+        assertEquals(true,testRanger instanceof Ranger);
+    }
 }
