ad4d2f357377128e2d44a8dde1c8f82c90b6c960
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-12 22:22:59 +0300

    add test for getter methods

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class AnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+
+   public void animal_instantiatesCorrectly_true() {
+
+        Animal testAnimal = new Animal(1, "Lion");
+        assertEquals(true,testAnimal instanceof Animal);
+    }
+
+    @Test
+
+    public void Animal_instantiatesWithRangerId_int(){
+
+        Animal testAnimal = new Animal(1, "Lion");
+        assertEquals(1,testAnimal.getRangerId());
+    }
+
+    @Test
+
+    public void Animal_instantiatesWithName_String() {
+
+        Animal testAnimal = new Animal(1, "Lion");
+        assertEquals("Lion",testAnimal.getName());
+    }
 }
