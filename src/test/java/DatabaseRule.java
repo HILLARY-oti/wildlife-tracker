54fae5e9bcdec5a429b083fc02c3edd5198a3aa2
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-12 21:03:11 +0300

    add change rule naming

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -7,14 +7,14 @@ public class DatabaseRule extends ExternalResource {
     @Override
 
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", null, null);
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "hillary", "hillary");
     }
 
     @Override
 
     protected void after() {
         try(Connection connect = DB.sql2o.open()) {
-            String deleteRangerQuery = "DELETE FROM rangers *;";
+            String deleteRangerQuery = "DELETE FROM ranger *;";
             connect.createQuery(deleteRangerQuery).executeUpdate();
         }
     }
