fdbff508a5d6e7d3b8fd5b555f3bcbfc72b6fd2e
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-12 10:20:59 +0300

    add override

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -8,7 +8,23 @@ public class Ranger {
         this.crew = crew;
     }
 
+    public String getName(){
+        return name;
+    }
+
     public String getCrew(){
         return crew;
+    }
+
+    @Override
+
+    public boolean equals(Object anotherRanger){
+        if(!(anotherRanger instanceof Ranger)){
+            return false;
+        } else {
+            Ranger newRanger = (Ranger) anotherRanger;
+            return this.getName().equals(newRanger.getName()) &&
+                    this.getCrew().equals(newRanger.getCrew());
+        }
     }
 }
