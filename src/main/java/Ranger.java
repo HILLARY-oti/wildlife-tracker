8dc279ad49d370057426267f465aafdd46b02bda
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-12 11:45:06 +0300

    add logic for methods save and all

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import org.sql2o.*;
+
 public class Ranger {
     private String name;
     private String crew;
@@ -8,23 +11,40 @@ public class Ranger {
         this.crew = crew;
     }
 
-    public String getName(){
+    public static List<Ranger> all() {
+        String sql = "SELECT*FROM rangers";
+        try (Connection connect = DB.sql2o.open()) {
+            return connect.createQuery(sql).executeAndFetch(Ranger.class);
+        }
+    }
+
+    public String getName() {
         return name;
     }
 
-    public String getCrew(){
+    public String getCrew() {
         return crew;
     }
 
     @Override
 
-    public boolean equals(Object anotherRanger){
-        if(!(anotherRanger instanceof Ranger)){
+    public boolean equals(Object anotherRanger) {
+        if (!(anotherRanger instanceof Ranger)) {
             return false;
         } else {
             Ranger newRanger = (Ranger) anotherRanger;
             return this.getName().equals(newRanger.getName()) &&
                     this.getCrew().equals(newRanger.getCrew());
+        }
+    }
+
+    public void save() {
+        try (Connection connect = DB.sql2o.open()) {
+            String sql = "INSERT INTO rangers(name, crew) VALUES (:name, :crew)";
+            connect.createQuery(sql)
+                    .addParameter("name", this.name)
+                    .addParameter("crew", this.crew)
+                    .executeUpdate();
         }
     }
 }
