f9b188def4a775ce5721dce84c8ce80290eae6f9
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-12 09:46:50 +0300

    add crew property and getter method

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -1,2 +1,14 @@
 public class Ranger {
+    private String name;
+    private String crew;
+
+    public Ranger(String name, String crew) {
+
+        this.name = name;
+        this.crew = crew;
+    }
+
+    public String getCrew(){
+        return crew;
+    }
 }
