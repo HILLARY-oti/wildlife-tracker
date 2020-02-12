0e01da468bde139a30fa71cb0628d358605b3a76
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-12 22:22:17 +0300

    add properties and getter methods

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,2 +1,19 @@
 public class Animal {
+
+    private int rangerId;
+    private String name;
+
+    public Animal(int rangerId, String name){
+
+        this.rangerId = rangerId;
+        this.name = name;
+    }
+
+    public int getRangerId(){
+        return rangerId;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
